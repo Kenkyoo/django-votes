e186c0c91c0d8fd083135f550bff6aecd7c21cc0
Franco <frfantusatti@protonmail.com>
2026-01-28 03:22:53 -0300

    upload files

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from polls.models import TbSubject, TbTeacher
+
+class SubjectModelAdmin(admin.ModelAdmin):
+    list_display = ('no', 'name', 'intro', 'is_hot')
+    search_fields = ('name', )
+    ordering = ('no', )
+
+
+class TeacherModelAdmin(admin.ModelAdmin):
+    list_display = ('no', 'name', 'sex', 'birth', 'good_count', 'bad_count', 'subject')
+    search_fields = ('name', )
+    ordering = ('no', )
+
+admin.site.register(TbSubject, SubjectModelAdmin)
+admin.site.register(TbTeacher, TeacherModelAdmin)
